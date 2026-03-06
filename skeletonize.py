727c472f5b3abe9125d1cf95c9d7acd13dc4c170
koladik <koladik@gmail.com>
2026-03-06 04:28:05 +0300

    skeletonize storage update

--- skeletonize.py
+++ skeletonize.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 from pygradskeleton import grayscale_skeletonize
-
+from storage_manager import Storage
 
 # image = cv2.imread("path/to/image")
 # result_1 = grayscaleskelet(image, method="KIM")
@@ -11,14 +11,16 @@ from pygradskeleton import grayscale_skeletonize
 def main():
 	root_path = Path("/media/koladik/HardDisk/Image")
 	image_folder = root_path / "ESRI_cut02_5m"
-	edges = get_edges(image_folder)
-	edges = edges[:, :, 0]
-	edges[edges < 125] = 0
+
+	edges_path = Path("test_images/test_01_edges.png")
+	storage = Storage.from_edges_path(edges_path)
+	edges = storage.load_edges()
+
+	#edges[edges < 125] = 0
 	edges_thin = grayscale_skeletonize(edges, h=125).astype(np.uint8)
 	_, edges_thick = cv2.threshold(edges, 125, 255, cv2.THRESH_BINARY)
 
-	edges_thin = cv2.merge((edges_thin, edges_thin, edges_thin))
-	save_thin_edges(image_folder, edges_thin)
+	storage.save_thin_edges(edges_thin)
 
 	fig = plt.figure(figsize=(8, 6))
 	axs = [
@@ -38,19 +40,6 @@ def main():
 
 	plt.show()
 
-
-def get_edges(image_folder: Path):
-	edge_path = image_folder.name + "_edge"
-	edge_path = image_folder / str(edge_path)
-	edge_path = edge_path.with_suffix(".png")
-	return cv2.imread(str(edge_path))
-
-
-def save_thin_edges(image_folder: Path, image: np.ndarray):
-	edge_path = image_folder.name + "_edge_thin"
-	edge_path = image_folder / str(edge_path)
-	edge_path = edge_path.with_suffix(".png")
-	cv2.imwrite(edge_path, image)
 
 
 if __name__ == "__main__":
