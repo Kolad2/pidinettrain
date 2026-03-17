1266ad00e0bb300ddc8d591eb2eeff645ab1df90
koladik <koladik@gmail.com>
2026-03-17 13:18:54 +0300

    pipline vectorize update

--- scripts_pipeline/task3_vectorize.py
+++ scripts_pipeline/task3_vectorize.py
@@ -1,15 +1,24 @@
 import cv2
+import numpy as np
 from pathlib import Path
-from rocknetmanager.tools.vectorize import vectorize
 from storage_manager import Storage
+from storage_manager.tools import save_shplines
+from skelet_vectorize.extract_lines import extract_lines
 
 
 def main():
-	thin_edges_path = Path("../test_images/test_01_thin_edges.png")
+	thin_edges_path = Path("D:/PycharmProjects/pidinettrain/test_images/test_01_pink_thin_edges.png")
+	#thin_edges_path = Path("test_images/test_01_thin_edges.png")
 	storage = Storage.from_thin_edges_path(thin_edges_path)
-	edges = storage.load_thin_edges()
-	polylines = vectorize(edges, storage.folder_path / "edges")
+	image_edges = storage.load_thin_edges()
+
+	polylines = extract_lines(image_edges)
+
+	save_shplines("D:/PycharmProjects/pidinettrain/test_images/test", polylines)
+
 
 
 if __name__ == "__main__":
 	main()
+
+
--- scripts_pipeline_test/vectorize.py
+++ scripts_pipeline_test/vectorize.py
@@ -1,19 +1,19 @@
 import cv2
+import numpy as np
 from pathlib import Path
 from storage_manager import Storage
 from storage_manager.tools import save_shplines
+from skelet_vectorize.extract_lines import extract_lines
 
-import numpy as np
 
 def main():
 	thin_edges_path = Path("D:/PycharmProjects/pidinettrain/test_images/test_01_pink_thin_edges.png")
 	#thin_edges_path = Path("test_images/test_01_thin_edges.png")
 	storage = Storage.from_thin_edges_path(thin_edges_path)
 	image_edges = storage.load_thin_edges()
-	from extract_lines import extract_lines
 	polylines = extract_lines(image_edges)
-
 	save_shplines("D:/PycharmProjects/pidinettrain/test_images/test", polylines)
+
 
 if __name__ == "__main__":
 	main()
